1195722bd34e9b670b01348a93fd2d63e05c3e5a
Edson Santos <cmo.edson@gmail.com>
2023-01-31 14:09:13 -0300

    Inserção de usuário com POST

--- src/main/java/com/edsannn/workshopmongo/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/edsannn/workshopmongo/resource/exception/ResourceExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.edsannn.workshopmongo.resource.exception;
 
-import javax.net.ssl.SSLEngineResult.Status;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
