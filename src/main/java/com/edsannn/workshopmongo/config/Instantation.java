b3f2c8239df23ff4fc4c543f31ba70f5722ade15
Edson Santos <cmo.edson@gmail.com>
2023-02-01 13:59:57 -0300

    Referenciando os posts do usuário

--- src/main/java/com/edsannn/workshopmongo/config/Instantation.java
+++ src/main/java/com/edsannn/workshopmongo/config/Instantation.java
@@ -43,6 +43,8 @@ public class Instantation implements CommandLineRunner {
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 	}
 
 	
