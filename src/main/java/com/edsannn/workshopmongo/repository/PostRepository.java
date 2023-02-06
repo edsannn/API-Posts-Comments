c90189334087715354d88d8d8510db5d7cb07476
Edson Santos <cmo.edson@gmail.com>
2023-02-06 13:37:24 -0300

    Consulta simples com query methods

--- src/main/java/com/edsannn/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/edsannn/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.edsannn.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.edsannn.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
+
+
